c8a125a1b3496080738c551579e72324be508522
Alberto Iglesias <alberto.iglesias.a@gmail.com>
2018-01-12 11:13:27 +0100

    refactor(demo): Modify the demo abc notation example

--- demo/src/index.js
+++ demo/src/index.js
@@ -5,11 +5,11 @@ import Abc2Svg from '../../src'
 
 class Demo extends Component {
   state = {
-    abcNotation: 'X:1\nT:Test\nM:4/4\nC:Trad.\nK:G\n|:GABc dedB|dedB dedB|c2ec B2dB|c2A2 A2BA|'
+    abcNotation: 'X:1\nT:Test\nM:4/4\nC:Trad.\nK:G\n|:GABc dedB'
   }
   componentDidMount() {
     setTimeout(() => {
-      this.setState({ abcNotation: 'X:1\nT:Test\nM:4/4\nC:Trad.\nK:G\n|:Gccc dedB|dedB dedB|c2ec B2dB|c2A2 A2BA|' })
+      this.setState({ abcNotation: 'X:1\nT:Test\nM:4/4\nC:Trad.\nK:G\n|:GcAB dedB|dedB dedB|c2ec B2dB|c2A2 A2BA|' })
     }, 5000)
   }
   render() {
