7c792c1612d38a04b43f71fb1275346b1afcdd4e
Alberto Iglesias <alberto.iglesias.a@gmail.com>
2018-02-04 09:44:45 +0100

    style: Small changes

--- demo/src/index.js
+++ demo/src/index.js
@@ -5,23 +5,25 @@ import Abc2Svg from '../../src'
 
 class Demo extends Component {
   state = {
-    abcNotation: 'X:1\nT:Test\nM:4/4\nK:C\n|:GcAB dedB|dedB dedB|c2ec B2dB|c2A2 A2BA|'
+    abcNotation:
+      'X:1\nT:Test\nM:4/4\nK:C\n|:GcAB dedB|dedB dedB|c2ec B2dB|c2A2 A2BA|'
   }
   componentDidMount() {
     setTimeout(() => {
-      this.setState({ abcNotation: 'X:1\nT:Test\nM:4/4\nK:C\n|:Gccc dedB|dedB dedB|c2ec B2dB|c2A2 A2BA|' })
+      this.setState({
+        abcNotation:
+          'X:1\nT:Test\nM:4/4\nK:C\n|:Gccc dedB|dedB dedB|c2ec B2dB|c2A2 A2BA|'
+      })
     }, 5000)
   }
   render() {
     const { abcNotation } = this.state
     return (
       <div style={{ border: '1px solid black', padding: '100px' }}>
-        <Abc2Svg
-          abcNotation={abcNotation}
-        />
+        <Abc2Svg abcNotation={abcNotation} />
       </div>
     )
   }
 }
 
-render(<Demo/>, document.querySelector('#demo'))
+render(<Demo />, document.querySelector('#demo'))
