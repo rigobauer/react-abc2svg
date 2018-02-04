dd056b9b853372e733bffb8d2aa9a43b6cc1fe14
Alberto Iglesias <alberto.iglesias.a@gmail.com>
2018-02-04 09:46:15 +0100

    style: More small changes (just testing prettier + husky + lint-staged)

--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import PropTypes from 'prop-types'
 import { Abc } from 'abc2svg/abc2svg-1.js'
 
 class Abc2svgCallbacks {
-
   constructor() {
-    this.abc_svg_output = ""
-    this.abc_error_output = ""
-    this.img_out = (data) => { this.abc_svg_output += data; }
-    this.errmsg = (msg, ln, cn) => { this.abc_error_output += msg + "<br/>\n"; }
-    this.read_file = (fn) => ""
+    this.abc_svg_output = ''
+    this.abc_error_output = ''
+    this.img_out = data => {
+      this.abc_svg_output += data
+    }
+    this.errmsg = (msg, ln, cn) => {
+      this.abc_error_output += msg + '<br/>\n'
+    }
+    this.read_file = fn => ''
     this.anno_start = (music_type, start_offset, stop_offset, x, y, w, h) => {}
     this.anno_stop = (music_type, start_offset, stop_offset, x, y, w, h) => {}
     this.get_abcmodel = (tsfirst, voice_tb, music_types, info) => {}
@@ -19,21 +22,23 @@ class Abc2svgCallbacks {
 }
 
 class Abc2Svg extends PureComponent {
-
   state = {
     containerWidth: ''
   }
-  
+
   uniqueNumber = Date.now() + Math.random()
   abcCallbacks = new Abc2svgCallbacks()
 
   fitWidth = () => {
-    this.setState({ containerWidth: '%%pagewidth ' + window.getComputedStyle(findDOMNode(this)).width + '\n'})    
+    this.setState({
+      containerWidth:
+        '%%pagewidth ' + window.getComputedStyle(findDOMNode(this)).width + '\n'
+    })
   }
 
   componentDidMount() {
     this.fitWidth()
-    window.addEventListener('resize', this.fitWidth);
+    window.addEventListener('resize', this.fitWidth)
   }
 
   componentWillUnmount() {
@@ -42,27 +47,36 @@ class Abc2Svg extends PureComponent {
 
   render() {
     const { abcNotation, showErrors } = this.props
-    const { containerWidth } = this.state    
+    const { containerWidth } = this.state
 
     let abc = new Abc(this.abcCallbacks)
     this.abcCallbacks.abc_svg_output = ''
     this.abcCallbacks.abc_error_output = ''
-    abc.tosvg('ABC NOTATION', '%%fullsvg ra2s' + this.uniqueNumber + '\n' + containerWidth + abcNotation)
+    abc.tosvg(
+      'ABC NOTATION',
+      '%%fullsvg ra2s' + this.uniqueNumber + '\n' + containerWidth + abcNotation
+    )
 
     return (
-      <div style={{ width: '100%' }} >
-        <div className="abc2svg-result" style={{ width: '100%' }}
+      <div style={{ width: '100%' }}>
+        <div
+          className="abc2svg-result"
+          style={{ width: '100%' }}
           dangerouslySetInnerHTML={{ __html: this.abcCallbacks.abc_svg_output }}
         />
-        {showErrors && this.abcCallbacks.abc_error_output !== '' &&
-          <div className="abc2svg-errors" style={{ width: '100%', textAlign: 'center' }}
-            dangerouslySetInnerHTML={{ __html: this.abcCallbacks.abc_error_output }}
-          />
-        }
+        {showErrors &&
+          this.abcCallbacks.abc_error_output !== '' && (
+            <div
+              className="abc2svg-errors"
+              style={{ width: '100%', textAlign: 'center' }}
+              dangerouslySetInnerHTML={{
+                __html: this.abcCallbacks.abc_error_output
+              }}
+            />
+          )}
       </div>
     )
   }
-
 }
 
 Abc2Svg.propTypes = {
